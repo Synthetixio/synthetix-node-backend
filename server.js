2b7682aeef55cb9387170732af374d35eb89b9b6
Volodymyr Derunov <vderunov@gmail.com>
2024-03-06 17:43:59 +0200

    Add crypto randomBytes and store logic

--- server.js
+++ server.js
@@ -1,20 +1,72 @@
 const express = require('express');
+const ethers = require('ethers');
+const crypto = require('crypto');
+const path = require('path');
+const { promises: fs } = require('fs');
 const app = express();
-const PORT = process.env.PORT || 3000;
+
+const PORT = process.env.PORT || 3005;
+const DATA_DIR = path.join(__dirname, 'data');
 
 app.use(express.json());
 
-app.post('/signup', (req, res) => {
-  const { walletAddress } = req.body;
-
-  if (!walletAddress) {
-    return res.status(400).send({ message: 'Missing wallet address' });
+const validateWalletAddress = (req, res) => {
+  if (!req.body.walletAddress) {
+    res.status(400).send({ message: 'Missing wallet address' });
+    return false;
   }
+  if (!ethers.isAddress(req.body.walletAddress)) {
+    res.status(400).send({ message: 'Invalid wallet address' });
+    return false;
+  }
+  return true;
+};
 
-  // Additional logic will be involved here
+const walletAddressStored = async (filePath) => {
+  try {
+    await fs.access(filePath);
+    return true;
+  } catch {
+    return false;
+  }
+};
 
-  console.log(walletAddress);
-  res.status(200).send({ message: 'Signup successful' });
+const generateRandomHexString = async () => {
+  return new Promise((resolve) => {
+    crypto.randomBytes(256, (err, buf) => {
+      if (err) throw err;
+      resolve(buf.toString('hex'));
+    });
+  });
+};
+
+const storeWalletAddress = async (fileName, fileContent) => {
+  try {
+    await fs.mkdir(DATA_DIR, { recursive: true });
+    await fs.writeFile(path.join(DATA_DIR, fileName), fileContent);
+  } catch (error) {
+    console.error(`Failed to save data to file: ${error}`);
+    throw error;
+  }
+};
+
+app.post('/signup', async (req, res) => {
+  if (!validateWalletAddress(req, res)) return;
+
+  const filePath = path.join(DATA_DIR, req.body.walletAddress);
+  try {
+    if (await walletAddressStored(filePath)) {
+      res.status(200).send({ signature: await fs.readFile(filePath, 'utf8') });
+      return;
+    }
+
+    const randomBytes = await generateRandomHexString();
+    await storeWalletAddress(req.body.walletAddress, randomBytes);
+
+    res.status(200).send({ signature: randomBytes });
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
 });
 
 app.listen(PORT, () => {
