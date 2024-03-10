9cc7d3f4c79312dc22ded493b19683460c896087
Volodymyr Derunov <vderunov@gmail.com>
2024-03-10 18:51:44 +0200

    Add .env.example file

--- server.js
+++ server.js
@@ -111,7 +111,7 @@ const manageWalletAddressStorage = async (req, res, next) => {
 
 const createJwtToken = async (walletAddress) => {
   return new Promise((resolve, reject) => {
-    jwt.sign({ walletAddress }, process.env.SECRET_KEY, { expiresIn: '1d' }, (err, token) => {
+    jwt.sign({ walletAddress }, process.env.JWT_SECRET_KEY, { expiresIn: '1d' }, (err, token) => {
       if (err) reject(err);
       resolve(token);
     });
@@ -136,7 +136,7 @@ const authenticateToken = (req, res, next) => {
   const authHeader = req.headers.authorization;
   const token = authHeader?.split(' ')[1];
   if (token == null) return res.sendStatus(401);
-  jwt.verify(token, process.env.SECRET_KEY, (err) => {
+  jwt.verify(token, process.env.JWT_SECRET_KEY, (err) => {
     if (err) return res.sendStatus(403);
     next();
   });
