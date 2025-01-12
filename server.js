d4e40f241e9438c9bae31705c6e3e81b8ba1550f
Volodymyr Derunov <vderunov@gmail.com>
2025-01-12 03:36:05 +0200

    Add IPFS stats and peers endpoints

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cp = require('node:child_process');
 const { ethers, JsonRpcProvider, Contract } = require('ethers');
 const { address, abi } = require('@vderunov/whitelist-contract/deployments/11155420/Whitelist');
 const crypto = require('node:crypto');
@@ -16,8 +17,126 @@ const IPFS_URL = `http://${IPFS_HOST}:${IPFS_PORT}/`;
 const GRAPH_API_ENDPOINT =
   'https://api.studio.thegraph.com/query/71164/vd-practice-v1/version/latest';
 
+const state = {
+  peers: [],
+  uptime: 0,
+  numObjects: 0,
+  repoSize: 0,
+  totalIn: 0,
+  totalOut: 0,
+  dailyIn: 0,
+  hourlyIn: 0,
+  dailyOut: 0,
+  hourlyOut: 0,
+};
+
 app.use(cors());
 app.use(express.json());
+
+app.get('/api', (_req, res) => {
+  res.status(200).json(state);
+});
+
+async function updateStats() {
+  try {
+    const { RepoSize: repoSize, NumObjects: numObjects } = await (
+      await fetch(`${IPFS_URL}api/v0/repo/stat`, { method: 'POST' })
+    ).json();
+    Object.assign(state, { repoSize, numObjects });
+  } catch (e) {
+    console.error(e);
+  }
+
+  try {
+    const { TotalIn: totalIn, TotalOut: totalOut } = await (
+      await fetch(`${IPFS_URL}api/v0/stats/bw`, { method: 'POST' })
+    ).json();
+    Object.assign(state, { totalIn, totalOut });
+  } catch (e) {
+    console.error(e);
+  }
+
+  const [pid] = await new Promise((resolve) =>
+    cp.exec('pgrep -f "ipfs daemon"', (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+        return resolve(null);
+      }
+      if (stderr) {
+        console.error(new Error(stderr));
+        return resolve(null);
+      }
+      return resolve(
+        stdout
+          .split('\n')
+          .map((s) => s.trim())
+          .filter(Boolean)
+      );
+    })
+  );
+  if (!pid) {
+    return;
+  }
+
+  const uptime = await new Promise((resolve) =>
+    cp.exec(`ps -p ${pid} -o lstart=`, (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+        return resolve(null);
+      }
+      if (stderr) {
+        console.error(new Error(stderr));
+        return resolve(null);
+      }
+      const startDate = new Date(stdout);
+      const uptimeInSeconds = Math.floor((Date.now() - startDate.getTime()) / 1000);
+      return resolve(uptimeInSeconds);
+    })
+  );
+  if (!uptime) {
+    return;
+  }
+  Object.assign(state, { uptime });
+
+  const uptimeHours = uptime / (60 * 60);
+  const uptimeDays = uptimeHours / 24;
+  const dailyIn = state.totalIn / uptimeDays;
+  const hourlyIn = state.totalIn / uptimeHours;
+  const dailyOut = state.totalOut / uptimeDays;
+  const hourlyOut = state.totalOut / uptimeHours;
+  Object.assign(state, { dailyIn, hourlyIn, dailyOut, hourlyOut });
+  console.log(state);
+}
+
+async function updatePeers() {
+  const peers = await new Promise((resolve) =>
+    cp.exec("ipfs-cluster-ctl --enc=json peers ls | jq '[inputs]'", (err, stdout, stderr) => {
+      if (err) {
+        console.error(err);
+        return resolve([]);
+      }
+      if (stderr) {
+        console.error(new Error(stderr));
+        return resolve([]);
+      }
+      try {
+        const result = JSON.parse(stdout);
+        return resolve(
+          result
+            .map(({ id, version }) => ({ id, version }))
+            .sort((a, b) => a.id.localeCompare(b.id))
+        );
+      } catch (_e) {
+        return resolve([]);
+      }
+    })
+  );
+  Object.assign(state, { peers });
+  console.log('[Peers Updated]', state);
+}
+
+setInterval(updatePeers, 60_000);
+setInterval(updateStats, 60_000);
 
 class HttpError extends Error {
   constructor(message, code) {
@@ -76,6 +195,7 @@ app.post('/signup', validateWalletAddress, transformWalletAddress, createNonce);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+  Promise.all([updateStats(), updatePeers()]);
 });
 
 const validateVerificationParameters = (req, _res, next) => {
